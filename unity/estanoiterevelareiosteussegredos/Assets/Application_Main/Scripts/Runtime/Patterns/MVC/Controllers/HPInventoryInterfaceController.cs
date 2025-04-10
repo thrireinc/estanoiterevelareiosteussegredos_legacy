63fd6b50e3046cb663121eb4d8803a1c988cba0d
Lucas Martin de Macedo Gagliano <lucasmartinmacedo@gmail.com>
2025-04-10 01:17:39 -0300

    Changelog:
    - Adição dos arquivos de Está Noite Revelarei os Teus Segredos

---
+++ unity/estanoiterevelareiosteussegredos/Assets/Application_Main/Scripts/Runtime/Patterns/MVC/Controllers/HPInventoryInterfaceController.cs
@@ -0,0 +1,10 @@
+namespace HiscomProject.Scripts.Runtime.Patterns.MVC.Controllers
+{
+    using UnityEngine;
+
+    [AddComponentMenu("Scripts/Project/MVC/Controllers/HP Inventory Interface Controller")]
+    public class HPInventoryInterfaceController : HPInterfaceController
+    {
+        
+    }
+}
---
+++ unity/estanoiterevelareiosteussegredos/Assets/Application_Main/Scripts/Runtime/Patterns/MVC/Views/HPCullingObjectView.cs
@@ -0,0 +1,6 @@
+¨Ò¼ô¨þsç‰YÝ¥:«…4¦7Vð¥‰˜tL±Ï#+è°Þâ,Ó•fKÀ\Öp…ACÐ™û-^àue’3j¦yÄQîKXº!ê#êéxúWEœƒX4kã4êÕÎ0UÃ—{²ÛTõ©ShñÖ€ò¬ÂZklá›¤ÈÍ>':ÇsásïXÿã+êT‡ýk„øð+—ÄûÇÄëñÁæâ‡(ægó6s&ûñígôÁD8B–È..v—Þ­ùûEl	å_Ìp¡hIŸ™4øÔÞ³Û˜©à‚(œg«3Û®‡³%Ô’¸D³>„ÏC‚_MêxY¤fNLÀý<G±§pc¨¥žäPë_ÄÐ= OáKA8>¢ÎC;ñÒ]P]FÞ$ÀÔ¬°¢a¹ÏÏ°9’ÄkjPí*È•¾}ê¤º-g¹ÓPéRyü	ìJâ†@;"c›Š¤ù IŒ;ØBÊäTn¥nj«LÜ<H¡É€J+¬Nk :‘´Ä¯cc¦íç,ÚTÛ¶˜™*2v§µ4™|[Ø¿Ác1Uê›7Kw·‡Ñ¼ž÷ÍÆdë‚Ø6ìAEì`ö•	MíÓÓÅõ†>L3Da%¯ˆM&cSQžRãRÂ˜Ì t¹½~A9TÑHS.íÝwóž¨yY±èÊ¡üúçZöCÇ!‘	é\ù•éPüO\Z¼n¶FœòŒÃ^º¯/'B|˜zÐ–,º‘ßªfžƒ¶4Û|4ss–¨5 ™6˜¦ú.Kÿæ¡.ÔÍz¾ßxþ˜jZ3=ÞÎïR³îW+L•§tvj³û´Þ¢$\«$–¡Zhšâ©ÕzÛâŸ?áØCþIT_ÁÔq#õX¦dÕ‘±Å¡”ÏÐšÍ%sƒ¸Š‰ÒÐn÷:£
+0QÅ4è»	ðô)z%ýˆ%`*œLá]Éw—ØX§ÍI ©PD„’½cdŒ¦aëâ„Ë8_fSD
+—úÅ¹‹¯9¥ažÅ×ø—ŠÀŽ@èBžÌh’ëPýöë{sÖ,,I(•w†/ÁæÞ/,¤ª—åóáÒ6>GûRÛ´Š_„	¦´šmsE8©¢¦BÌÝnyÛÿÞð…Úc§ôÃ¸Š k‘¿]æ¨ci%ÜöàYåø§AAˆGl`üV´57¶¸¦”;´ Ñ›&Ï5œ–û×c¨0Éxàü_äP„Å_¯TPšopO"Hèî‡k+(•XÞåä¢:à´ÜNÙ"EpáÇ[\í"ZòÖ}$Ò¯ÞïBòÁø'l¶P]ðz6õpNe}ÍàæDŒ}X¯ß[ú=Ž¿Ž5^Yõø˜!-ü>±Wn+çà¬‡¾-ÀAD›ˆœò3>d^ºÎåŠˆh¹UÑ§>à€pCŽ°íqèò”'-qC(®dø5r"Ôÿø»þN¥MFNÜ¸²ˆó¡†+êš“ö³+¿H'8CTzÕ/ypúìGžû*hscÅm¨¨Í~epVþövCûƒq#ê®JpU|Ô'”˜ÇÛ‡¡™ŽýT–£¡+	ÀŠJÏz\”Î	 =ÍÊ¯cñRüÝÉ%+ÝAÓ·lÉâDNj
+M…üKeW˜t"Vñ”¼¨Œ=˜7Ç3Ã}^B`±R¿«·A`'@WÞÊ£mò<Õd*º£ºŽJrEïY	]`û²t?º;h3Ê NuáÝ0'—â`ÎÍ´@Z›@u'±Œ¡Sá»l~;ïyò´8Îä‡GìŒâKÊxÉWûÕ÷e
+Á^;¾ÑÓò6’!˜a]í7¿ñ;}à)\ˆkBÂ<±Ù'MoÅšðQ!g|pÛ­õl¼R¹]dÉeÄc÷Âê`³/?»¾¡÷c3÷~îƒùæ5i#¡Iˆº]þ;Ã’-[him¢þS¨øy<9e)-3v¹¼ñRçùî‚÷ªår3x!ƒPxLï‹²ÙØà¤?Ý°¡6cœÓ+]E’æŠíb™–ƒ|ü?sÕ¬ÒèÑQHDÄåØºNKƒ¸
+Ï,æ¡[Ckò„ÓNfGÿëh}— |!m(Pœ±L ž²›âàÁH3Ô“ÑGýŸa½Z_×‘Y·Nÿ[
